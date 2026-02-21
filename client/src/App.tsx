e7252b4958d46ec59fd1ff9fd5bbcc50d63aa981
Rohit Singh <rr2488017@gmail.com>
2026-02-21 20:07:33 +0530

    Starts frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Pricing from "./pages/Pricing.tsx";
+import Projects from "./pages/Projects.tsx";
+import MyProjects from "./pages/MyProjects.tsx";
+import Preview from "./pages/Preview.tsx";
+import Community from "./pages/Community.tsx";
+import View from "./pages/View.tsx";
+import Navbar from "./components/Navbar.tsx";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/preview/:projectId" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/view/:projectId" element={<View />} />
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
